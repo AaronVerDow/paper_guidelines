459a8fd3482ef7970a7494d7c5a67db10731d3ba
Aaron VerDow <aaron@verdow.com>
2025-04-05 16:47:40 -0500

    choose color

--- src/render.sh
+++ src/render.sh
@@ -3,12 +3,14 @@ set -exuo pipefail
 
 openscad-render dotted_paper.scad
 
+color=${1:-#f0f0f0}
+
 # pkg librsvg
 
 for input in $( find ./output/ | grep 'svg$'); do
 	output=${input//svg/pdf}
 	sed -i 's/stroke="black"//g' "$input"
-	# sed -i 's/lightgray/#f0f0f0/g' "$input"
+	sed -i "s/lightgray/$color/g" "$input"
 	rsvg-convert -f pdf -o "$output" "$input"
 	rm "$input"
 done
